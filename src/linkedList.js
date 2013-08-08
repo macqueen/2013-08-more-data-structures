38337c228f3abd32aacb2caff11b1bcb8d3de523
Hack Reactor Students <github@hackreactor.com>
2013-08-07 21:09:13 -0700

    added functionality to addToTail in linkedList

--- src/linkedList.js
+++ src/linkedList.js
@@ -3,11 +3,27 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var indexer = 0;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+    if (list.head === null && list.tail === null){
+      list.head = list.tail = makeNode(value);
+    } else if (list.head === list.tail) {
+      list.tail = makeNode(value);
+      list.head.next = 'tail';
+    } else {
+      list[indexer-1].next = list[indexer];
+      list[indexer] = list.tail;
+      list[indexer].next = 'tail';
+      indexer++;
+      list.tail = makeNode(value);
+    }
   };
 
   list.removeHead = function(){
+    var target = list[list.head.next];
+    delete list[list.head.next];
+    list.head = target;
   };
 
   list.contains = function(){
@@ -16,7 +32,7 @@ var makeLinkedList = function(){
   return list;
 };
 
-var makeNode = function(value){
+var makeNode = function(value, pointer){
   var node = {};
   node.value = value;
   node.next = null;
