72bd7cd9d2a358bda117893f6d82d9996d2e8717
Hack Reactor Students <github@hackreactor.com>
2013-08-08 17:01:31 -0700

    added functionality for addChild method

--- src/tree.js
+++ src/tree.js
@@ -1,14 +1,29 @@
-var makeTree = function(){
-  var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
 
+// .children property, an array containing a number of subtrees
+// .addChild() method, takes any value, sets that as the target of a node,
+  //and adds that node as a child of the tree
+// A .contains() method, takes any input and returns a boolean reflecting
+  //whether it can be found as the value of the target node
+
+
+var makeTree = function(value){
+  var newTree = {};
+  newTree.value = value;
+  newTree.children = [];
+  extend(newTree, treeMethods);
   return newTree;
+};
+
+var extend = function(obj1, obj2){
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(){
