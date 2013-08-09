80259402c89ae98b428518fa227b65f5c125619e
Hack Reactor Students <github@hackreactor.com>
2013-08-08 17:00:43 -0700

    added tests for .addChild method

--- spec/treeSpec.js
+++ spec/treeSpec.js
@@ -11,5 +11,14 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
+  it("should add to children after addChild is called", function() {
+    tree.addChild('first');
+    expect(tree.children[0].value).toEqual('first');
+    tree.addChild('second');
+    expect(tree.children[1].value).toEqual('second');
+    tree.children[0].addChild('first');
+    expect(tree.children[0].children[0].value).toEqual('first');
+  });
+
   // Add more tests here to test the functionality of tree.
 });
