0ba860f8b6b05054caaf346509fd5082dbb82afd
Hack Reactor Students <github@hackreactor.com>
2013-08-09 17:00:08 -0700

    fixed tests for binary search tree insert

--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe("binarySearchTree", function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree =  new BinarySearchTree();
+    binarySearchTree =  new BinarySearchTree(10);
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
@@ -12,11 +12,21 @@ describe("binarySearchTree", function() {
   });
 
   it("should place higher values to the right", function() {
-    binarySearchTree.insert(10);
     binarySearchTree.insert(15);
     binarySearchTree.insert(20);
+    binarySearchTree.insert(25);
     expect(binarySearchTree.right.value).toEqual(15);
     expect(binarySearchTree.right.right.value).toEqual(20);
+    expect(binarySearchTree.right.right.right.value).toEqual(25);
+  });
+
+  it("should place lower values to the left", function() {
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(3);
+    expect(binarySearchTree.left.value).toEqual(7);
+    expect(binarySearchTree.left.left.value).toEqual(5);
+    expect(binarySearchTree.left.left.left.value).toEqual(3);
   });
 
 });
